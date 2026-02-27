0321b9ee06bd6914a3da53de9ac24ea4153517bd
linentrackcrm <edwardtabb@xxconstruction.net>
2026-02-27 02:52:49 -0600

    Update app.php

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -12,8 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->api(prepend: [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+    $middleware->api(prepend: [
+        \Illuminate\Http\Middleware\HandleCors::class,
+        \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+    
         ]);
 
         $middleware->alias([
